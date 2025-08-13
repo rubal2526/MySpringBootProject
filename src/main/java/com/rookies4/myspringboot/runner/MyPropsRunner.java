d22494e78b285f6e58b74902b93749d3b07ff7de
rubal2526 <pig121512141@gmail.com>
2025-08-13 10:02:26 +0900

    property 추가

--- src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -1,13 +1,66 @@
 package com.rookies4.myspringboot.runner;
 
+import com.rookies4.myspringboot.property.MyBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 @Component
 public class MyPropsRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private String age;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyBootProperties properties;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyBootProperties.getName() = " + properties.getName());
+        System.out.println("MyBootProperties.getAge() = " + properties.getAge());
+        System.out.println("MyBootProperties.getFullName() = " + properties.getFullName());
+
+        System.out.println("Properties myboot.name = " + name);
+        System.out.println("Properties myboot.age = " + age);
+        System.out.println("Properties myboot.fullName = " + environment.getProperty("myboot.fullName"));
+
+        System.out.println("VM Arguments = " + args.containsOption("foo")); //false
+        System.out.println("Program Arguments = " + args.containsOption("bar")); // true
+
+        //Program Argument의 모든 이름을 출력하기
+        for(String argName: args.getOptionNames()){
+            System.out.println("아규먼트 = " + argName);
+        }
+        //args.getOptionNames() 의 리턴 타입 Ser<String>
+        //Iterable의 foreach(Consumer) 메서드 호출하기
+        //Consumer의 추상메서드 void accept(T t)
+        //1. 익명의 Inner class ( Anonymous Inner Class)
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("Inner Class 아규먼트 = " + s);
+            }
+        });
+        System.out.println("===> 람다함수");
+        //2. 함수형 인터페이스 (람다 함수)
+        args.getOptionNames().forEach(name -> System.out.println(name));
+        System.out.println("===> Method Reference");
+        //3. Method Reference (야규먼트를 생략한 람다함수)
+        args.getOptionNames().forEach(System.out::println);
+
+
+
+
 
     }
 }
