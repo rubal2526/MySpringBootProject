f3e0f07d59089e1a616af26267f1fc26557aa38d
rubal2526 <pig121512141@gmail.com>
2025-08-18 14:52:30 +0900

    html add

--- src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -1,11 +1,27 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.entity.UserEntity;
+import com.rookies4.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/users/index")
+    public ModelAndView index() {
+        List<UserEntity> userEntityList = userRepository.findAll();
+        return new ModelAndView("index", "users", userEntityList);
+    }
+
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트");
