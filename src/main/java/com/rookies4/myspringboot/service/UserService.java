72b4034160f5d42ebf82be863010d29bb4310c90
rubal2526 <pig121512141@gmail.com>
2025-08-20 10:51:04 +0900

    UserService update

--- src/main/java/com/rookies4/myspringboot/service/UserService.java
+++ src/main/java/com/rookies4/myspringboot/service/UserService.java
@@ -1,11 +1,26 @@
 package com.rookies4.myspringboot.service;
 
+import com.rookies4.myspringboot.controller.dto.UserDTO;
+import com.rookies4.myspringboot.exception.BusinessException;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+
+    //User 등록
+    @Transactional
+    public final UserDTO.UserResponse createUser(UserDTO.UserCreateRequest request) {
+        //Email 중복검사
+        userRepository.findByEmail(request.getEmail()) //Optional<UserEntity>
+                .ifPresent(entity -> {
+                    throw new BusinessException("User with this Email already Exist", HttpStatus.CONFLICT)
+                });
+    }
 }
