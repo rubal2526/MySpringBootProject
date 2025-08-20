7195ed534a2fc40ab1987d5b8fc9c9a913329319
rubal2526 <pig121512141@gmail.com>
2025-08-20 11:12:46 +0900

    UserCon add

---
+++ src/main/java/com/rookies4/myspringboot/controller/UserServiceRestController.java
@@ -0,0 +1,26 @@
+package com.rookies4.myspringboot.controller;
+
+import com.rookies4.myspringboot.controller.dto.UserDTO;
+import com.rookies4.myspringboot.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserServiceRestController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO.UserResponse> create(
+            @Valid @RequestBody UserDTO.UserCreateRequest request) {
+        UserDTO.UserResponse createUser = userService.createUser(request);
+        return ResponseEntity.ok(createUser);
+
+    }
+}
